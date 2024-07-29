4b841ce4a87eb6a567acf01438dc083304ba0b8d
Vìctor Quiñones Nevarez <VictorQuinones@iMac-de-Mario.local>
2024-07-28 22:43:24 -0600

    agregue la carpeta auth y el archivo Auth.jsx

---
+++ src/auth/Auth.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Auth() {
+  return (
+    <div>Auth</div>
+  )
+}
+
+export default Auth
